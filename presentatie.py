579e3906156ff98160420c169431b48710e39902
Melanie472 <melanie@de-ru.net>
2024-11-06 16:51:45 +0100

    Update presentatie.py

--- presentatie.py
+++ presentatie.py
@@ -27,7 +27,7 @@ st.title("Analyse vliegtuigen")
 @st.cache_data # Zorgt ervoor dat de functie in dit geval maar 1 keer wordt uitgevoerd
 def get_data(): # Haalt alle data op
 
-    url = 'https://drive.google.com/uc?id=JOUWBESTANDS-ID'
+    url = 'https://www.dropbox.com/scl/fi/xn5z9gscg7uft90c3jf3f/schedule_airport.gzip?rlkey=msphztdto9ngidn0oh34wqgkq&st=obphj0zl&dl=0'
     output = 'lokaal_bestand.csv'
     gdown.download(url, output, quiet=False)  # Leest het CSV-bestand
 
